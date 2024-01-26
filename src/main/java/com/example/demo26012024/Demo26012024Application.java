5bf7fe812c519ffc9f709322cfa1d6126eebf69e
Hristo Kyosev <143337714+CuriousItsy@users.noreply.github.com>
2024-01-26 16:22:30 +0100

    this is the first commit

---
+++ src/main/java/com/example/demo26012024/Demo26012024Application.java
@@ -0,0 +1,13 @@
+package com.example.demo26012024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo26012024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo26012024Application.class, args);
+    }
+
+}
